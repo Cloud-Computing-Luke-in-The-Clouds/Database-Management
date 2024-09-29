2261c12d3edf2163e6dd10307c2625eeac5bd56d
Hung-Ching Liu <b05902043@ntu.edu.tw>
2024-09-29 16:31:57 -0400

    feat: modify researcher profile schema

--- researcher-profile/researcher.sql
+++ researcher-profile/researcher.sql
@@ -1,20 +1,30 @@
 CREATE DATABASE  researcher_database;
 
 use researcher_database;
-create table if not exists researcher
-(
-    researcher_id       bigint not null,
-    user_id             bigint not null,
-    field_of_study      text   null,
-    school_organization text   null,
-    citation_count      int   null,
-    present             bool   not null
+CREATE TABLE IF NOT EXISTS ResearchProfile (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    google_scholar_link VARCHAR(255),
+    personal_website_link VARCHAR(255),
+    organization VARCHAR(255),
+    title VARCHAR(255),
+    age INT,
+    sex VARCHAR(10)
 );
 
-insert into researcher_database.researcher
-    value (12345,
-           67890,
-           "Computer Science",
-           "Columbia University",
-           "24",
-           false);
+INSERT INTO ResearchProfile (google_scholar_link, personal_website_link, organization, title, age, sex) VALUES (
+    'https://scholar.google.com/citations?user=l2g4PFYAAAAJ&hl=en',
+    'https://www.chingyitsai.com/',
+    'Princeton University',
+    'Ph.D. Student',
+    27,
+    'Male');
+
+INSERT INTO ResearchProfile (google_scholar_link, personal_website_link, organization, title, age, sex) VALUES (
+    'https://scholar.google.com.tw/citations?user=ykuVSuEAAAAJ',
+    'https://tsunghan-wu.github.io/',
+    'UC Berkeley',
+    'Ph.D. Student',
+    27,
+    'Male');
+
+select * from ResearchProfile;
