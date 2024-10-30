41a6131d20e66787351bad0040ab862a317e32c2
Hung-Ching Liu <b05902043@ntu.edu.tw>
2024-10-29 22:50:33 -0400

    feat(researcher): add user name, image url to Research Profile table

--- researcher-profile/researcher.sql
+++ researcher-profile/researcher.sql
@@ -3,6 +3,8 @@ CREATE DATABASE  researcher_database;
 use researcher_database;
 CREATE TABLE IF NOT EXISTS ResearchProfile (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    researcher_name VARCHAR(255),
+    image_url VARCHAR(255),
     google_scholar_link VARCHAR(255),
     personal_website_link VARCHAR(255),
     organization VARCHAR(255),
@@ -11,7 +13,9 @@ CREATE TABLE IF NOT EXISTS ResearchProfile (
     sex VARCHAR(10)
 );
 
-INSERT INTO ResearchProfile (google_scholar_link, personal_website_link, organization, title, age, sex) VALUES (
+INSERT INTO ResearchProfile (researcher_name, image_url, google_scholar_link, personal_website_link, organization, title, age, sex) VALUES (
+    "ChinYi Tsai",
+    "https://randomuser.me/api/portraits/men/32.jpg"
     'https://scholar.google.com/citations?user=l2g4PFYAAAAJ&hl=en',
     'https://www.chingyitsai.com/',
     'Princeton University',
@@ -19,7 +23,9 @@ INSERT INTO ResearchProfile (google_scholar_link, personal_website_link, organiz
     27,
     'Male');
 
-INSERT INTO ResearchProfile (google_scholar_link, personal_website_link, organization, title, age, sex) VALUES (
+INSERT INTO ResearchProfile (researcher_name, image_url, google_scholar_link, personal_website_link, organization, title, age, sex) VALUES (
+    "TsungHan Wu",
+    "https://randomuser.me/api/portraits/women/28.jpg",
     'https://scholar.google.com.tw/citations?user=ykuVSuEAAAAJ',
     'https://tsunghan-wu.github.io/',
     'UC Berkeley',
@@ -27,7 +33,9 @@ INSERT INTO ResearchProfile (google_scholar_link, personal_website_link, organiz
     27,
     'Male');
 
-INSERT INTO ResearchProfile (google_scholar_link, personal_website_link, organization, title, age, sex) VALUES (
+INSERT INTO ResearchProfile (researcher_name, image_url, google_scholar_link, personal_website_link, organization, title, age, sex) VALUES (
+    "Emma Watson",
+    "https://randomuser.me/api/portraits/women/68.jpg",
     'google_scholar.com',
     'personal_website.com',
     'Columbia University',
